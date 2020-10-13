d4878f3a74ce33142e5dc780bce82c0604e5ee58
snekitsing <snekitsing@yahoo.co.uk>
2020-10-13 13:27:07 +0100

    .

--- Back-End/Back-EndMain.cs
+++ Back-End/Back-EndMain.cs
@@ -19,55 +19,58 @@ namespace Back_End
         static void Main(string[] args)
         {
         }
-   
 
-    //Fetch new bookings : Seb
-    private void fetchData()
-    {
-        
-    }
-    
-    //Batch update the secondary database : Seb
-    private void batchUpdate()
-    {
-        
-    }
 
-    //Update Primary database with new bookings : Sing
-    private void updatePrimaryDatabase()
-    {
-        query("",false);
+        //Fetch new bookings : Seb
+        private void fetchData()
+        {
 
-    }
+        }
 
-    //Query database for information : Sing
-    private void queryDatabase()
-    {
-    
-         query("",false);
+        //Batch update the secondary database : Seb
+        private void batchUpdate()
+        {
 
-    }
+        }
 
-    //Send print notification to front-end : Akmal
-    private void sendPrintNotifications(){
+        //Update Primary database with new bookings : Sing
+        private void updatePrimaryDatabase()
+        {
+            query("", false);
 
-    }
+        }
 
-    //Batch recovery/resync in case of batch failure : Ndey
-    private void batchRecovery(){
-    
-    }
+        //Query database for information : Sing
+        private void queryDatabase()
+        {
 
-    //Notify front-end of bacth recovery : Ndey
-    private void notifyRecovery(){
-    
-    }
-        
-    
+            query("", false);
 
-    //Sing : Query Databases Function - Accept query and returns boolean value.
+        }
 
-         public bool query(string query, bool flag)
+        //Send print notification to front-end : Akmal
+        private void sendPrintNotifications()
+        {
+
+        }
+
+        //Batch recovery/resync in case of batch failure : Ndey
+        private void batchRecovery()
+        {
+
+        }
+
+        //Notify front-end of bacth recovery : Ndey
+        private void notifyRecovery()
+        {
+
+        }
+
+
+
+        //Sing : Query Databases Function - Accept query and returns boolean value.
+
+        public bool query(string query, bool flag)
         {
             try
             {
@@ -79,11 +82,29 @@ namespace Back_End
             }
             catch (Exception error)
             {
-               // connection.Close();
+                // connection.Close();
                 MessageBox.Show(error.Message.ToString());
                 return flag = false;
             }
         }
 
+
+        //Sing : Login for Front-end.
+        private void authenticateUser(string username, string password)
+        {
+            if (username == "admin" && password == "password")
+            {
+                //athenticate admin
+            }
+            else if (username == "normaluser" && password == "normalpassword")
+            {
+                //athenticate normal user
+            }
+            else
+            {
+                //username and password is wrong - try again.
+                MessageBox.Show("Username or password is incorrrect, please try again");
+            };
+        }
     }
 }
