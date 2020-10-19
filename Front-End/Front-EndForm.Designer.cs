9c01bc956f8d8e174291b24ab05e5c92cc5fb931
kevinsulatra <ksulatra@gmail.com>
2020-10-19 21:47:06 +0100

    Add code for application exit

--- Front-End/Front-EndForm.Designer.cs
+++ Front-End/Front-EndForm.Designer.cs
@@ -28,13 +28,37 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.extButton = new System.Windows.Forms.Button();
+            this.userSearch = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // extButton
+            // 
+            this.extButton.Location = new System.Drawing.Point(12, 405);
+            this.extButton.Name = "extButton";
+            this.extButton.Size = new System.Drawing.Size(138, 33);
+            this.extButton.TabIndex = 0;
+            this.extButton.Text = "Exit Application";
+            this.extButton.UseVisualStyleBackColor = true;
+            this.extButton.Click += new System.EventHandler(this.extButton_Click);
+            // 
+            // userSearch
+            // 
+            this.userSearch.Location = new System.Drawing.Point(330, 167);
+            this.userSearch.Name = "userSearch";
+            this.userSearch.Size = new System.Drawing.Size(138, 33);
+            this.userSearch.TabIndex = 1;
+            this.userSearch.Text = "Holiday Search";
+            this.userSearch.UseVisualStyleBackColor = true;
+            this.userSearch.Click += new System.EventHandler(this.userSearch_Click);
             // 
             // main
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.userSearch);
+            this.Controls.Add(this.extButton);
             this.Name = "main";
             this.Text = "Holiday Booking System";
             this.Load += new System.EventHandler(this.main_Load);
@@ -43,6 +67,9 @@
         }
 
         #endregion
+
+        private System.Windows.Forms.Button extButton;
+        private System.Windows.Forms.Button userSearch;
     }
 }
 
--- Front-End/Front-EndForm.cs
+++ Front-End/Front-EndForm.cs
@@ -24,5 +24,25 @@ namespace Front_End
         {
 
         }
+
+        private void extButton_Click(object sender, EventArgs e)
+        {
+            const string text = "Do you want to exit?";
+            const string caption = "EXIT";
+            var result = MessageBox.Show(text, caption, MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (result == DialogResult.Yes)
+            {
+                Application.Exit();
+            }
+            else if (result == DialogResult.Yes)
+            {
+                this.Close();
+            }
+        }
+
+        private void userSearch_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
