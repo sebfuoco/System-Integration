d3501fd6e0dc41f3beaea33de3f7712108d18658
nikhil250497 <72796684+nikhil250497@users.noreply.github.com>
2020-11-17 13:44:39 +0000

    update print invoice

--- Front-End/Print-Invoice.Designer.cs
+++ Front-End/Print-Invoice.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace Front_End
+﻿0namespace Front_End
 {
     partial class Print_Invoice
     {
@@ -30,13 +30,15 @@
         {
             this.prntInvButton = new System.Windows.Forms.Button();
             this.rtnButtom = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // prntInvButton
             // 
-            this.prntInvButton.Location = new System.Drawing.Point(650, 405);
+            this.prntInvButton.Location = new System.Drawing.Point(731, 506);
+            this.prntInvButton.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.prntInvButton.Name = "prntInvButton";
-            this.prntInvButton.Size = new System.Drawing.Size(138, 33);
+            this.prntInvButton.Size = new System.Drawing.Size(155, 41);
             this.prntInvButton.TabIndex = 4;
             this.prntInvButton.Text = "Print Invoice";
             this.prntInvButton.UseVisualStyleBackColor = true;
@@ -44,25 +46,38 @@
             // 
             // rtnButtom
             // 
-            this.rtnButtom.Location = new System.Drawing.Point(12, 405);
+            this.rtnButtom.Location = new System.Drawing.Point(14, 506);
+            this.rtnButtom.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.rtnButtom.Name = "rtnButtom";
-            this.rtnButtom.Size = new System.Drawing.Size(158, 33);
+            this.rtnButtom.Size = new System.Drawing.Size(178, 41);
             this.rtnButtom.TabIndex = 5;
             this.rtnButtom.Text = "Return to Homepage";
             this.rtnButtom.UseVisualStyleBackColor = true;
             this.rtnButtom.Click += new System.EventHandler(this.rtnButtom_Click);
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(125, 48);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 20);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "label1";
+            // 
             // Print_Invoice
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(900, 562);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.rtnButtom);
             this.Controls.Add(this.prntInvButton);
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.Name = "Print_Invoice";
             this.Text = "Invoice";
             this.Load += new System.EventHandler(this.Print_Invoice_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -70,5 +85,6 @@
 
         private System.Windows.Forms.Button prntInvButton;
         private System.Windows.Forms.Button rtnButtom;
+        private System.Windows.Forms.Label label1;
     }
 }
