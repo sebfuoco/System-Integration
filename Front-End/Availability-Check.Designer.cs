f1c34e8dc60f7755a426d22a6af9ec64db5f1c7c
Devon Walker <20201195@farn-ct.ac.uk>
2020-12-03 20:36:33 +0000

    button test

--- Front-End/Availability-Check.Designer.cs
+++ Front-End/Availability-Check.Designer.cs
@@ -28,180 +28,231 @@
         /// </summary>
         private void InitializeComponent()
         {
-			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Availability_Check));
-			this.extButton = new System.Windows.Forms.Button();
-			this.nxtButton = new System.Windows.Forms.Button();
-			this.cnlButton = new System.Windows.Forms.Button();
-			this.label2 = new System.Windows.Forms.Label();
-			this.DateTimePicker1 = new System.Windows.Forms.DateTimePicker();
-			this.Check = new System.Windows.Forms.Button();
-			this.Printb = new System.Windows.Forms.Button();
-			this.Copyb = new System.Windows.Forms.Button();
-			this.Locationinput = new System.Windows.Forms.TextBox();
-			this.Avaoutput = new System.Windows.Forms.TextBox();
-			this.Seatoutput = new System.Windows.Forms.TextBox();
-			this.label1 = new System.Windows.Forms.Label();
-			this.label3 = new System.Windows.Forms.Label();
-			this.label4 = new System.Windows.Forms.Label();
-			this.SuspendLayout();
-			// 
-			// extButton
-			// 
-			this.extButton.BackColor = System.Drawing.Color.Transparent;
-			this.extButton.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.extButton.Location = new System.Drawing.Point(577, -1);
-			this.extButton.Margin = new System.Windows.Forms.Padding(2, 1, 2, 1);
-			this.extButton.Name = "extButton";
-			this.extButton.Size = new System.Drawing.Size(24, 23);
-			this.extButton.TabIndex = 16;
-			this.extButton.Text = "X";
-			this.extButton.UseVisualStyleBackColor = false;
-			this.extButton.Click += new System.EventHandler(this.extButton_Click_1);
-			// 
-			// nxtButton
-			// 
-			this.nxtButton.Image = ((System.Drawing.Image)(resources.GetObject("nxtButton.Image")));
-			this.nxtButton.Location = new System.Drawing.Point(535, 436);
-			this.nxtButton.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
-			this.nxtButton.Name = "nxtButton";
-			this.nxtButton.Size = new System.Drawing.Size(56, 42);
-			this.nxtButton.TabIndex = 14;
-			this.nxtButton.UseVisualStyleBackColor = true;
-			this.nxtButton.Click += new System.EventHandler(this.nxtButton_Click_1);
-			// 
-			// cnlButton
-			// 
-			this.cnlButton.Image = ((System.Drawing.Image)(resources.GetObject("cnlButton.Image")));
-			this.cnlButton.Location = new System.Drawing.Point(9, 436);
-			this.cnlButton.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
-			this.cnlButton.Name = "cnlButton";
-			this.cnlButton.Size = new System.Drawing.Size(43, 42);
-			this.cnlButton.TabIndex = 13;
-			this.cnlButton.UseVisualStyleBackColor = true;
-			this.cnlButton.Click += new System.EventHandler(this.cnlButton_Click_2);
-			// 
-			// label2
-			// 
-			this.label2.AutoSize = true;
-			this.label2.Location = new System.Drawing.Point(248, 9);
-			this.label2.Name = "label2";
-			this.label2.Size = new System.Drawing.Size(90, 13);
-			this.label2.TabIndex = 18;
-			this.label2.Text = "Availability Check";
-			// 
-			// DateTimePicker1
-			// 
-			this.DateTimePicker1.Location = new System.Drawing.Point(278, 56);
-			this.DateTimePicker1.Name = "DateTimePicker1";
-			this.DateTimePicker1.Size = new System.Drawing.Size(200, 20);
-			this.DateTimePicker1.TabIndex = 19;
-			// 
-			// Check
-			// 
-			this.Check.Location = new System.Drawing.Point(403, 82);
-			this.Check.Name = "Check";
-			this.Check.Size = new System.Drawing.Size(75, 23);
-			this.Check.TabIndex = 20;
-			this.Check.Text = "Check";
-			this.Check.UseVisualStyleBackColor = true;
-			// 
-			// Printb
-			// 
-			this.Printb.Location = new System.Drawing.Point(308, 221);
-			this.Printb.Name = "Printb";
-			this.Printb.Size = new System.Drawing.Size(75, 23);
-			this.Printb.TabIndex = 21;
-			this.Printb.Text = "Print";
-			this.Printb.UseVisualStyleBackColor = true;
-			this.Printb.Click += new System.EventHandler(this.Printb_Click);
-			// 
-			// Copyb
-			// 
-			this.Copyb.Location = new System.Drawing.Point(212, 221);
-			this.Copyb.Name = "Copyb";
-			this.Copyb.Size = new System.Drawing.Size(75, 23);
-			this.Copyb.TabIndex = 22;
-			this.Copyb.Text = "Copy";
-			this.Copyb.UseVisualStyleBackColor = true;
-			this.Copyb.Click += new System.EventHandler(this.Copyb_Click);
-			// 
-			// Locationinput
-			// 
-			this.Locationinput.Location = new System.Drawing.Point(170, 56);
-			this.Locationinput.Name = "Locationinput";
-			this.Locationinput.Size = new System.Drawing.Size(100, 20);
-			this.Locationinput.TabIndex = 23;
-			// 
-			// Avaoutput
-			// 
-			this.Avaoutput.Location = new System.Drawing.Point(187, 184);
-			this.Avaoutput.Name = "Avaoutput";
-			this.Avaoutput.Size = new System.Drawing.Size(100, 20);
-			this.Avaoutput.TabIndex = 24;
-			this.Avaoutput.TextChanged += new System.EventHandler(this.Avaoutput_TextChanged);
-			// 
-			// Seatoutput
-			// 
-			this.Seatoutput.Location = new System.Drawing.Point(303, 184);
-			this.Seatoutput.Name = "Seatoutput";
-			this.Seatoutput.Size = new System.Drawing.Size(100, 20);
-			this.Seatoutput.TabIndex = 25;
-			this.Seatoutput.TextChanged += new System.EventHandler(this.Seatoutput_TextChanged);
-			// 
-			// label1
-			// 
-			this.label1.AutoSize = true;
-			this.label1.Location = new System.Drawing.Point(319, 166);
-			this.label1.Name = "label1";
-			this.label1.Size = new System.Drawing.Size(64, 13);
-			this.label1.TabIndex = 26;
-			this.label1.Text = "Seats taken";
-			// 
-			// label3
-			// 
-			this.label3.AutoSize = true;
-			this.label3.Location = new System.Drawing.Point(184, 451);
-			this.label3.Name = "label3";
-			this.label3.Size = new System.Drawing.Size(219, 13);
-			this.label3.TabIndex = 27;
-			this.label3.Text = "Results may not be represintitive of live sever";
-			// 
-			// label4
-			// 
-			this.label4.AutoSize = true;
-			this.label4.Location = new System.Drawing.Point(209, 166);
-			this.label4.Name = "label4";
-			this.label4.Size = new System.Drawing.Size(48, 13);
-			this.label4.TabIndex = 28;
-			this.label4.Text = "Avaliblity";
-			// 
-			// Availability_Check
-			// 
-			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.BackColor = System.Drawing.Color.White;
-			this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
-			this.ClientSize = new System.Drawing.Size(600, 487);
-			this.Controls.Add(this.label4);
-			this.Controls.Add(this.label3);
-			this.Controls.Add(this.label1);
-			this.Controls.Add(this.Seatoutput);
-			this.Controls.Add(this.Avaoutput);
-			this.Controls.Add(this.Locationinput);
-			this.Controls.Add(this.Copyb);
-			this.Controls.Add(this.Printb);
-			this.Controls.Add(this.Check);
-			this.Controls.Add(this.DateTimePicker1);
-			this.Controls.Add(this.label2);
-			this.Controls.Add(this.extButton);
-			this.Controls.Add(this.nxtButton);
-			this.Controls.Add(this.cnlButton);
-			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
-			this.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
-			this.Name = "Availability_Check";
-			this.Text = "Availability Check";
-			this.ResumeLayout(false);
-			this.PerformLayout();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Availability_Check));
+            this.extButton = new System.Windows.Forms.Button();
+            this.nxtButton = new System.Windows.Forms.Button();
+            this.cnlButton = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.DateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.Check = new System.Windows.Forms.Button();
+            this.Printb = new System.Windows.Forms.Button();
+            this.Copyb = new System.Windows.Forms.Button();
+            this.Avaoutput = new System.Windows.Forms.TextBox();
+            this.Seatoutput = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.Flightdetails = new System.Windows.Forms.GroupBox();
+            this.Cardetails = new System.Windows.Forms.GroupBox();
+            this.Hoteldetails = new System.Windows.Forms.GroupBox();
+            this.Locationinput = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // extButton
+            // 
+            this.extButton.BackColor = System.Drawing.Color.Transparent;
+            this.extButton.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.extButton.Location = new System.Drawing.Point(577, -1);
+            this.extButton.Margin = new System.Windows.Forms.Padding(2, 1, 2, 1);
+            this.extButton.Name = "extButton";
+            this.extButton.Size = new System.Drawing.Size(24, 23);
+            this.extButton.TabIndex = 16;
+            this.extButton.Text = "X";
+            this.extButton.UseVisualStyleBackColor = false;
+            this.extButton.Click += new System.EventHandler(this.extButton_Click_1);
+            // 
+            // nxtButton
+            // 
+            this.nxtButton.Image = ((System.Drawing.Image)(resources.GetObject("nxtButton.Image")));
+            this.nxtButton.Location = new System.Drawing.Point(535, 436);
+            this.nxtButton.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.nxtButton.Name = "nxtButton";
+            this.nxtButton.Size = new System.Drawing.Size(56, 42);
+            this.nxtButton.TabIndex = 14;
+            this.nxtButton.UseVisualStyleBackColor = true;
+            this.nxtButton.Click += new System.EventHandler(this.nxtButton_Click_1);
+            // 
+            // cnlButton
+            // 
+            this.cnlButton.Image = ((System.Drawing.Image)(resources.GetObject("cnlButton.Image")));
+            this.cnlButton.Location = new System.Drawing.Point(9, 436);
+            this.cnlButton.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.cnlButton.Name = "cnlButton";
+            this.cnlButton.Size = new System.Drawing.Size(43, 42);
+            this.cnlButton.TabIndex = 13;
+            this.cnlButton.UseVisualStyleBackColor = true;
+            this.cnlButton.Click += new System.EventHandler(this.cnlButton_Click_2);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(248, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(90, 13);
+            this.label2.TabIndex = 18;
+            this.label2.Text = "Availability Check";
+            // 
+            // DateTimePicker1
+            // 
+            this.DateTimePicker1.Location = new System.Drawing.Point(278, 56);
+            this.DateTimePicker1.Name = "DateTimePicker1";
+            this.DateTimePicker1.Size = new System.Drawing.Size(200, 20);
+            this.DateTimePicker1.TabIndex = 19;
+            // 
+            // Check
+            // 
+            this.Check.Location = new System.Drawing.Point(403, 82);
+            this.Check.Name = "Check";
+            this.Check.Size = new System.Drawing.Size(75, 23);
+            this.Check.TabIndex = 20;
+            this.Check.Text = "Check";
+            this.Check.UseVisualStyleBackColor = true;
+            this.Check.Click += new System.EventHandler(this.Check_Click_1);
+            // 
+            // Printb
+            // 
+            this.Printb.Location = new System.Drawing.Point(305, 119);
+            this.Printb.Name = "Printb";
+            this.Printb.Size = new System.Drawing.Size(75, 23);
+            this.Printb.TabIndex = 21;
+            this.Printb.Text = "Print";
+            this.Printb.UseVisualStyleBackColor = true;
+            this.Printb.Click += new System.EventHandler(this.Printb_Click);
+            // 
+            // Copyb
+            // 
+            this.Copyb.Location = new System.Drawing.Point(224, 119);
+            this.Copyb.Name = "Copyb";
+            this.Copyb.Size = new System.Drawing.Size(75, 23);
+            this.Copyb.TabIndex = 22;
+            this.Copyb.Text = "Copy";
+            this.Copyb.UseVisualStyleBackColor = true;
+            this.Copyb.Click += new System.EventHandler(this.Copyb_Click);
+            // 
+            // Avaoutput
+            // 
+            this.Avaoutput.Location = new System.Drawing.Point(12, 121);
+            this.Avaoutput.Name = "Avaoutput";
+            this.Avaoutput.Size = new System.Drawing.Size(100, 20);
+            this.Avaoutput.TabIndex = 24;
+            this.Avaoutput.TextChanged += new System.EventHandler(this.Avaoutput_TextChanged);
+            // 
+            // Seatoutput
+            // 
+            this.Seatoutput.Location = new System.Drawing.Point(118, 121);
+            this.Seatoutput.Name = "Seatoutput";
+            this.Seatoutput.Size = new System.Drawing.Size(100, 20);
+            this.Seatoutput.TabIndex = 25;
+            this.Seatoutput.TextChanged += new System.EventHandler(this.Seatoutput_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(134, 105);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(64, 13);
+            this.label1.TabIndex = 26;
+            this.label1.Text = "Seats taken";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(184, 451);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(219, 13);
+            this.label3.TabIndex = 27;
+            this.label3.Text = "Results may not be represintitive of live sever";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(38, 105);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(48, 13);
+            this.label4.TabIndex = 28;
+            this.label4.Text = "Avaliblity";
+            // 
+            // Flightdetails
+            // 
+            this.Flightdetails.Location = new System.Drawing.Point(9, 164);
+            this.Flightdetails.Name = "Flightdetails";
+            this.Flightdetails.Size = new System.Drawing.Size(176, 100);
+            this.Flightdetails.TabIndex = 31;
+            this.Flightdetails.TabStop = false;
+            this.Flightdetails.Text = "Flight Details";
+            this.Flightdetails.Enter += new System.EventHandler(this.Flightdetails_Enter_1);
+            // 
+            // Cardetails
+            // 
+            this.Cardetails.Location = new System.Drawing.Point(204, 164);
+            this.Cardetails.Name = "Cardetails";
+            this.Cardetails.Size = new System.Drawing.Size(176, 100);
+            this.Cardetails.TabIndex = 32;
+            this.Cardetails.TabStop = false;
+            this.Cardetails.Text = "Car Hire Details";
+            this.Cardetails.Enter += new System.EventHandler(this.Cardetails_Enter);
+            // 
+            // Hoteldetails
+            // 
+            this.Hoteldetails.Location = new System.Drawing.Point(394, 164);
+            this.Hoteldetails.Name = "Hoteldetails";
+            this.Hoteldetails.Size = new System.Drawing.Size(176, 100);
+            this.Hoteldetails.TabIndex = 32;
+            this.Hoteldetails.TabStop = false;
+            this.Hoteldetails.Text = "Hotel Details";
+            this.Hoteldetails.Enter += new System.EventHandler(this.Hoteldetails_Enter);
+            // 
+            // Locationinput
+            // 
+            this.Locationinput.FormattingEnabled = true;
+            this.Locationinput.Location = new System.Drawing.Point(151, 55);
+            this.Locationinput.Name = "Locationinput";
+            this.Locationinput.Size = new System.Drawing.Size(121, 21);
+            this.Locationinput.TabIndex = 33;
+            this.Locationinput.Text = "Please select one";
+            this.Locationinput.SelectedIndexChanged += new System.EventHandler(this.Locationinput_SelectedIndexChanged_1);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(388, 334);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 34;
+            this.button1.Text = "button test";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // Availability_Check
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(600, 487);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.Locationinput);
+            this.Controls.Add(this.Hoteldetails);
+            this.Controls.Add(this.Cardetails);
+            this.Controls.Add(this.Flightdetails);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Seatoutput);
+            this.Controls.Add(this.Avaoutput);
+            this.Controls.Add(this.Copyb);
+            this.Controls.Add(this.Printb);
+            this.Controls.Add(this.Check);
+            this.Controls.Add(this.DateTimePicker1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.extButton);
+            this.Controls.Add(this.nxtButton);
+            this.Controls.Add(this.cnlButton);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.Name = "Availability_Check";
+            this.Text = "Availability Check";
+            this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -215,11 +266,15 @@
         private System.Windows.Forms.Button Check;
         private System.Windows.Forms.Button Printb;
         private System.Windows.Forms.Button Copyb;
-        private System.Windows.Forms.TextBox Locationinput;
         private System.Windows.Forms.TextBox Avaoutput;
         private System.Windows.Forms.TextBox Seatoutput;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.GroupBox Flightdetails;
+        private System.Windows.Forms.GroupBox Cardetails;
+        private System.Windows.Forms.GroupBox Hoteldetails;
+        private System.Windows.Forms.ComboBox Locationinput;
+        private System.Windows.Forms.Button button1;
     }
 }
