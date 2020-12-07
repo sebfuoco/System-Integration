877f7732f2632316c5d6af0106c278bfa2392ab1
kevinsulatra <ksulatra@gmail.com>
2020-12-07 16:34:38 +0000

    Udate UX

--- Front-End/Front-EndForm.Designer.cs
+++ Front-End/Front-EndForm.Designer.cs
@@ -41,10 +41,9 @@
             // userSearch
             // 
             this.userSearch.Font = new System.Drawing.Font("Century Gothic", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.userSearch.Location = new System.Drawing.Point(534, 239);
-            this.userSearch.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.userSearch.Location = new System.Drawing.Point(475, 191);
             this.userSearch.Name = "userSearch";
-            this.userSearch.Size = new System.Drawing.Size(279, 60);
+            this.userSearch.Size = new System.Drawing.Size(248, 48);
             this.userSearch.TabIndex = 1;
             this.userSearch.Text = "Holiday Search";
             this.userSearch.UseVisualStyleBackColor = true;
@@ -53,10 +52,9 @@
             // mgrButton
             // 
             this.mgrButton.Font = new System.Drawing.Font("Century Gothic", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.mgrButton.Location = new System.Drawing.Point(534, 351);
-            this.mgrButton.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.mgrButton.Location = new System.Drawing.Point(475, 281);
             this.mgrButton.Name = "mgrButton";
-            this.mgrButton.Size = new System.Drawing.Size(279, 60);
+            this.mgrButton.Size = new System.Drawing.Size(248, 48);
             this.mgrButton.TabIndex = 2;
             this.mgrButton.Text = "Admin Log in";
             this.mgrButton.UseVisualStyleBackColor = true;
@@ -66,11 +64,10 @@
             // 
             this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(33, 55);
-            this.pictureBox1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.pictureBox1.Location = new System.Drawing.Point(86, -1);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(495, 528);
-            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.Size = new System.Drawing.Size(117, 127);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
             this.pictureBox1.TabIndex = 3;
             this.pictureBox1.TabStop = false;
             // 
@@ -78,10 +75,10 @@
             // 
             this.extButton.BackColor = System.Drawing.Color.Transparent;
             this.extButton.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.extButton.Location = new System.Drawing.Point(866, -1);
+            this.extButton.Location = new System.Drawing.Point(770, -1);
             this.extButton.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.extButton.Name = "extButton";
-            this.extButton.Size = new System.Drawing.Size(36, 36);
+            this.extButton.Size = new System.Drawing.Size(32, 29);
             this.extButton.TabIndex = 8;
             this.extButton.Text = "X";
             this.extButton.UseVisualStyleBackColor = false;
@@ -93,9 +90,9 @@
             this.label2.BackColor = System.Drawing.Color.Transparent;
             this.label2.Font = new System.Drawing.Font("Century Schoolbook", 17.25F, ((System.Drawing.FontStyle)(((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic) 
                 | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(235, 11);
+            this.label2.Location = new System.Drawing.Point(209, 9);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(452, 40);
+            this.label2.Size = new System.Drawing.Size(375, 34);
             this.label2.TabIndex = 9;
             this.label2.Text = "Holiday Booking System";
             // 
@@ -104,19 +101,19 @@
             this.label1.AutoSize = true;
             this.label1.BackColor = System.Drawing.Color.Transparent;
             this.label1.Font = new System.Drawing.Font("Brush Script MT", 22.2F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(122, 587);
+            this.label1.Location = new System.Drawing.Point(35, 364);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(304, 55);
+            this.label1.Size = new System.Drawing.Size(260, 47);
             this.label1.TabIndex = 10;
             this.label1.Text = "Perfect Get Away";
             // 
             // main
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
-            this.ClientSize = new System.Drawing.Size(900, 750);
+            this.ClientSize = new System.Drawing.Size(800, 600);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.pictureBox1);
@@ -124,7 +121,6 @@
             this.Controls.Add(this.mgrButton);
             this.Controls.Add(this.userSearch);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
-            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.Name = "main";
             this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
             this.Text = "Holiday Booking System";
